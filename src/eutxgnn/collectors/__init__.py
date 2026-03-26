6323057c168f1c1b179b36afafb7e2ec49067b3d
Light <lightman.chang@gmail.com>
2026-03-26 23:33:11 +0800

    Add drugs.json, fix collectors init

--- src/eutxgnn/collectors/__init__.py
+++ src/eutxgnn/collectors/__init__.py
@@ -1,12 +1,28 @@
 """Evidence collectors for EuTxGNN drug repurposing validation."""
 
 from .base import BaseCollector, CollectorResult
+from .bundle import BundleAggregator, CandidateInfo, EvidenceBundle
 from .clinicaltrials import ClinicalTrialsCollector
+from .drug_bundle import (
+    CollectionStatus,
+    DrugBundle,
+    DrugBundleAggregator,
+)
+from .drugbank import DrugBankCollector
+from .ictrp import ICTRPCollector
 from .pubmed import PubMedCollector
 
 __all__ = [
     "BaseCollector",
     "CollectorResult",
+    "BundleAggregator",
+    "CandidateInfo",
+    "EvidenceBundle",
     "ClinicalTrialsCollector",
+    "CollectionStatus",
+    "DrugBundle",
+    "DrugBundleAggregator",
+    "DrugBankCollector",
+    "ICTRPCollector",
     "PubMedCollector",
 ]
